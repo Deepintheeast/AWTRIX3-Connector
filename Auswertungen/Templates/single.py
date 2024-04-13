79b96b9e55898cac0551cecdba3a27a21135e7b1
Deepintheeast <136626582+Deepintheeast@users.noreply.github.com>
2024-04-13 13:00:55 +0200

    Update single.py

--- Auswertungen/Templates/single.py
+++ Auswertungen/Templates/single.py
@@ -1,1 +1,20 @@
+""" Auswertung Template für einen einzelnen Wert
+WERT und ICON müssen an die verwendeten, gewünschten Werte angepasst werden"""
 
+# Importieren der benötigten Funktionen aus der Funktionen-Bibliothek
+from funktionen import awtrix3_send_app
+
+def auswertung(app, data, config):
+    # Auswertung Template für einen einzelnen Wert
+    data_app = {
+        "text": str(int(data["WERT"])) + " %",
+        "icon": ICON,
+        "color": "#00ff00",
+    }
+    awtrix3_send_app(
+        config["awtrix3"]["url"],
+        app,
+        data_app,
+        config["settings"]["app_scroll_duration"],
+        config["settings"]["app_show_time"],
+    )
