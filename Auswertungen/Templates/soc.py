b0617337e30b6d9e434e82c219484e25b2d0ddb0
Deepintheeast <136626582+Deepintheeast@users.noreply.github.com>
2024-05-18 14:09:15 +0200

    Update soc.py

--- Auswertungen/Templates/soc.py
+++ Auswertungen/Templates/soc.py
@@ -6,33 +6,41 @@ from funktionen import awtrix3_send_app
 def auswertung(app, data, config):
     # Auswertung SOC mit unterschiedlichen Icons und Farben je nach Wert
     # und Anzeige des aktuellen SOC als Progressbar (Balken)
+
+    #Zuweisung Wert SOC
+    SOC = int(data["soc_Pylontech_SOC"])
+
+    #Auswahl des Icons und der Farbe je nach SOC-Wert
     icon = 0
     color = "#ad0b0b"
-    if int(float(data["soc_SOC"])) == 0:
+    if SOC == 0:
         icon = 12832
         color = "#ad0b0b"
-    elif int(float(data["soc_SOC"])) <= 21:
+    elif SOC <= 21:
         icon = 6359
         color = "#e56d17"
-    elif int(float(data["soc_SOC"])) <= 41:
+    elif SOC <= 41:
         icon = 6360
         color = "#d8a90c"
-    elif int(float(data["soc_SOC"])) <= 61:
+    elif SOC <= 61:
         icon = 6361
         color = "#f4ea1f"
-    elif int(float(data["soc_SOC"])) <= 81:
+    elif SOC <= 81:
         icon = 6362
         color = "#b6e61a"
-    elif int(float(data["soc_SOC"])) <= 99:
+    elif SOC <= 99:
         icon = 6363
         color = "#2cf046"
+    
+    #Aufbereitung der Daten für die Anzeige in der App
     data_app = {
-        "text": str(int(data["soc_SOC"])) + " %",
-        "progress": (data["soc_SOC"]),
+        "text": str(SOC) + " %",
+        "progress": SOC,
         "progressc": "#00ff00",
         "icon": icon,
         "color": color,
     }
+    # Senden der Daten an Awtrix3
     awtrix3_send_app(
         config["awtrix3"]["url"],
         app,
